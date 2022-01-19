37e076de610544e84ab0cbb113d10464022c0ee8
Reis-Joao <joaobernardoalmeidareis@gmail.com>
2022-01-19 17:13:16 +0100

    update

--- date.py
+++ date.py
@@ -1,10 +1,9 @@
 date = "2022-01-17"
 
-def compute_tomorrow(date):  
-    year = int(date[0:4])
-    month = int(date[5:7])
-    day = int(date[-2:])
-
+year = int(date[0:4])
+month = int(date[5:7])
+day = int(date[-2:])
+   
 print("Next class is on ", year, month, day + 1)
 
 
